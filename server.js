dc3bcf8e1605841bae076167c67cf8085989e64b
poojapatel28 <poojapatel28071998@gmail.com>
2017-09-06 12:03:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,24 +5,112 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles={
+    'article-one': {
+        title: 'Article One | Pooja',
+        heading: 'ARTICLE ONE',
+        date: '6 sept, 2016',
+        content:`
+          <p>
+               'This the content for my first article. I love to write a lot. So do read me and allow me to spread words and love.
+           </p>
+            <p>
+               This the content for my first article. I love to write a lot. So do read me and allow me to spread words and love.
+           </p>` 
+                },
+    'article-two': {
+         title: 'Article Two | Pooja',
+        heading: 'ARTICLE TWO',
+        date: '18 sept, 2016',
+        content:`
+          <p>
+           This the content for my second article. I love to write a lot. So do read me and allow me to spread words and love. Now in this article i'll tell you something about myself and how i co=ame to this lebel of life leaving behind all frustration and problems.
+       </p>
+       
+        <p>
+           Writing back to you is something more important then anything. So do wait for me guys with some interesting stoy down the lane.....
+       </p>` 
+    },
+    'article-three': {
+         title: 'Article Three | Pooja',
+        heading: 'ARTICLE THREE',
+        date: '20 sept, 2016',
+        content:`
+            <p>
+           This the content for my Third article. Here, I'll narrate you a story of a girl and a boy who used to be best friends but somehow circumstances made them a bit apart.
+       </p>
+       
+        <p>
+           Writing back to you is something more important then anything. So do wait for me guys with some interesting stoy down the lane.....
+       </p>` 
+    }
+   
+};
+
+
+function createTemplate(data)
+{
+
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmlTemplate= `
+          <html>
+
+<head>
+    <title>
+        ${title}
+    </title>
+    
+    <meta name="viewport" content="width=device-width, initial-salce=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+
+   
+</head>
+
+<body>
+    <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        
+        <hr/>
+        
+       <h3>
+          ${heading}
+       </h3>
+       
+       <div>
+           ${date}
+       </div>
+       
+       <div>
+          ${content}
+           
+       </div>
+   </div>
+</body>
+
+</html>`
+
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res)
+app.get('/:articleName',function(req,res)
 {
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
