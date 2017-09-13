f3263849049f340f5ee4b26e43da6a0ca955a3d5
poojapatel28 <poojapatel28071998@gmail.com>
2017-09-13 15:11:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res)
 {
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+  //  res.send(createTemplate(articles[articleName]));
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
