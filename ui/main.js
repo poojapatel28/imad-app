c7422ec433e07bd7629bc8f0238c0bdbaa52611d
poojapatel28 <poojapatel28071998@gmail.com>
2017-09-13 15:09:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
-element.innerHTML='new value'
+element.innerHTML='new value';
 
 var img=document.getElementById('img');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginleft+'px';
+};
 img.onClick=function()
 {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,50);
+   
 };
